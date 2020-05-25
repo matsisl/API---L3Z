230239ea8a63dd1a97f1e3d306518eba2b9d5fdf
matsisl <sislmatko@gmail.com>
2020-05-25 13:59:07 +0200

    Added database context
    Added db models and context

---
+++ Server side API/DAL/VehicleContext.cs
@@ -0,0 +1,16 @@
+namespace DAL
+{
+    using Model;
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class VehicleContext : DbContext
+    {
+        public DbSet<VehicleMake> VehicleMakes { get; set; }
+        public DbSet<VehicleModel> VehicleModels { get; set; }
+        public VehicleContext() : base("name=VehicleModel")
+        {
+        }
+    }
+}
